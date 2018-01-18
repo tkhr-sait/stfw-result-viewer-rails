bfe7c3eae6a7e4b38db4afbf2258b7d5188c24ed
tkhr-sait <tkhr-sait@users.noreply.github.com>
2018-01-18 16:37:34 +0900

    Initial commit

---
+++ db/migrate/20180118063038_create_postdata.rb
@@ -0,0 +1,10 @@
+class CreatePostdata < ActiveRecord::Migration[5.1]
+  def change
+    create_table :postdata do |t|
+      t.string :hookId
+      t.string :payload
+
+      t.timestamps
+    end
+  end
+end
